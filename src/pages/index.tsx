bf49dbd46a2e7a5648ded083077f2af0884fd432
RobsonSilveira <robson.silveira_@hotmail.com>
2021-07-09 15:54:11 -0300

    FEA: Commit day 1

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Header } from "../components/Header"
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <h1>Next!</h1>
+      <h2>Next!</h2>
+      <h3>Next!</h3>
+    </div>
+  )
+}
